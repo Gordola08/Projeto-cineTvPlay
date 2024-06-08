eea3de3bc7a328a12b4523f5e30cb0b6e9a0718e
air music completo <132394117+Gordola08@users.noreply.github.com>
2024-06-08 12:05:08 -0300

    CineTvPlay

--- cineTvPlay/js/filmes.js
+++ cineTvPlay/js/filmes.js
@@ -3,25 +3,70 @@ const apiUrl = 'https://api.themoviedb.org/3';
 const imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
 const embedderBaseUrl = 'https://embedder.net/e/';
 
+let currentPage = 1;
+
 document.addEventListener('DOMContentLoaded', () => {
   fetchMovies();
-  fetchSeries();
+  setupSearch();
+  setupDropdowns();
+  setupPagination();
 });
 
-function fetchMovies() {
-  let filmesHtml = []
-  for (let page = 1; page <= 5; page++) {
-    fetch(`${apiUrl}/movie/popular?api_key=${apiKey}&language=pt-BR&page=${page}`, {
-      method: 'GET',
-    })
-      .then(response => response.json())
-      .then(filmes => {
-        for (const key in filmes) {
-          filmesHtml = filmesHtml.concat(filmes.results);
-        }
-        displayContent(filmesHtml, 'movies-container')
-      })
+function fetchMovies(categoryId = null, page = 1) {
+  let moviesHtml = [];
+  let url = `${apiUrl}/movie/popular?api_key=${apiKey}&language=pt-BR&page=${page}`;
+  if (categoryId) {
+    url = `${apiUrl}/discover/movie?api_key=${apiKey}&language=pt-BR&with_genres=${categoryId}&page=${page}`;
   }
+  
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      moviesHtml = data.results;
+      displayContent(moviesHtml, 'movies-container');
+    })
+    .catch(error => {
+      console.error('Error fetching movies:', error);
+    });
+}
+
+function setupDropdowns() {
+  const categoryMenu = document.getElementById('categoryMenu');
+
+  categoryMenu.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', event => {
+      event.preventDefault();
+      const categoryId = event.target.getAttribute('data-category');
+      fetchMovies(categoryId);
+    });
+  });
+}
+
+function setupSearch() {
+  const searchButton = document.getElementById('button-addon2');
+  searchButton.addEventListener('click', () => {
+    const searchText = document.getElementById('searchInput').value;
+    if (searchText.trim() !== '') {
+      searchMovies(searchText);
+    } else {
+      fetchMovies();
+    }
+  });
+}
+
+function searchMovies(query) {
+  const encodedQuery = encodeURIComponent(query);
+  const searchUrl = `${apiUrl}/search/movie?api_key=${apiKey}&language=pt-BR&query=${encodedQuery}`;
+  
+  fetch(searchUrl)
+    .then(response => response.json())
+    .then(data => {
+      const results = data.results;
+      displayContent(results, 'movies-container');
+    })
+    .catch(error => {
+      console.error('Error searching movies:', error);
+    });
 }
 
 function displayContent(items, containerId) {
@@ -49,10 +94,35 @@ function watchVideo(id) {
 }
 
 
+function setupDropdowns() {
+  const categoryMenu = document.getElementById('categoryMenu');
+
+  categoryMenu.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', event => {
+      event.preventDefault();
+      const categoryId = event.target.getAttribute('data-category');
+      fetchMovies(categoryId);
+    });
+  });
+}
 
 
+function setupPagination() {
+  const prevPageButton = document.getElementById('prevPage');
+  const nextPageButton = document.getElementById('nextPage');
 
+  prevPageButton.addEventListener('click', () => {
+    if (currentPage > 1) {
+      currentPage--;
+      fetchMovies(null, currentPage);
+    }
+  });
 
+  nextPageButton.addEventListener('click', () => {
+    currentPage++;
+    fetchMovies(null, currentPage);
+  });
+}
 
 
 document.addEventListener("DOMContentLoaded", function () {
--- cineTvPlay/js/serie.js
+++ cineTvPlay/js/serie.js
@@ -3,26 +3,69 @@ const apiUrl = 'https://api.themoviedb.org/3';
 const imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
 const embedderBaseUrl = 'https://embedder.net/e/';
 
+let currentPage = 1;
+let currentCategory = null;
+let currentType = null;
+
 document.addEventListener('DOMContentLoaded', () => {
   fetchSeries();
+  setupPagination();
 });
 
+function fetchSeries(categoryId = null, type = null, page = 1) {
+  currentCategory = categoryId;
+  currentType = type;
+  currentPage = page;
+  let seriesHTML = [];
+  let url = `${apiUrl}/tv/popular?api_key=${apiKey}&language=pt-BR&page=${page}`;
 
-function fetchSeries() {
-  let seriesHTML = []
-  for (let page = 1; page <= 5; page++) {
-    fetch(`${apiUrl}/tv/popular?api_key=${apiKey}&language=pt-BR&page=${page}`, {
-      method: 'GET',
-    })
-      .then(response => response.json())
-      .then(filmes => {
-        for (const key in filmes) {
-          seriesHTML = seriesHTML.concat(filmes.results);
-        }
-        displayContent(seriesHTML, 'series-container')
-      })
+  if (categoryId) {
+    if (categoryId === '16' && type === 'anime') {
+      url = `${apiUrl}/discover/tv?api_key=${apiKey}&language=pt-BR&with_genres=${categoryId}&with_keywords=210024&page=${page}`; // Filtrando por Anime
+    } else if (categoryId === '16' && type === 'cartoon') {
+      url = `${apiUrl}/discover/tv?api_key=${apiKey}&language=pt-BR&with_genres=${categoryId}&without_keywords=210024&page=${page}`; // Filtrando por Desenho
+    } else if (categoryId === '35' && type === 'dorama') {
+      url = `${apiUrl}/discover/tv?api_key=${apiKey}&language=pt-BR&with_genres=${categoryId}&page=${page}`; // Filtrando por Dorama
+    } else if (categoryId === '28' || categoryId === '12') {
+      url = `${apiUrl}/discover/tv?api_key=${apiKey}&language=pt-BR&with_genres=${categoryId}&page=${page}`; // Filtrando por Ação ou Aventura
+    } else {
+      url = `${apiUrl}/discover/tv?api_key=${apiKey}&language=pt-BR&with_genres=${categoryId}&page=${page}`;
+    }
   }
+
+  fetch(url, {
+    method: 'GET',
+  })
+    .then(response => response.json())
+    .then(filmes => {
+      seriesHTML = seriesHTML.concat(filmes.results);
+      displayContent(seriesHTML, 'series-container');
+    });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetchSeries();
+  setupPagination();
+  setupSearch();
+  setupDropdowns();
+});
+
+function setupDropdowns() {
+  const categoryMenu = document.getElementById('categoryMenu');
+
+  categoryMenu.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', event => {
+      event.preventDefault();
+      const categoryName = event.target.textContent;
+      document.getElementById('dropdownMenuButton').textContent = categoryName;
+      const categoryId = event.target.getAttribute('data-category');
+      const type = event.target.getAttribute('data-type');
+      fetchSeries(categoryId, type, 1);
+    });
+  });
+}
+
+
 
 function displayContent(items, containerId) {
   const container = document.getElementById(containerId);
@@ -48,13 +91,67 @@ function watchVideo(id) {
   window.open(embedUrl, '_blank');
 }
 
+function setupPagination() {
+  document.getElementById('prevPage').addEventListener('click', event => {
+    event.preventDefault();
+    if (currentPage > 1) {
+      fetchSeries(currentCategory, currentType, currentPage - 1);
+    }
+  });
+
+  document.getElementById('nextPage').addEventListener('click', event => {
+    event.preventDefault();
+    fetchSeries(currentCategory, currentType, currentPage + 1);
+  });
+}
+
+document.querySelectorAll('.dropdown-item').forEach(item => {
+  item.addEventListener('click', event => {
+    event.preventDefault();
+    const categoryName = event.target.textContent;
+    document.getElementById('dropdownMenuButton').textContent = categoryName;
+    const categoryId = event.target.getAttribute('data-category');
+    const type = event.target.getAttribute('data-type');
+    fetchSeries(categoryId, type, 1);
+  });
+});
 
 
+document.addEventListener('DOMContentLoaded', () => {
+  fetchSeries();
+  setupPagination();
+  setupSearch();
+});
 
+function setupDropdowns() {
+  const categoryMenu = document.getElementById('categoryMenu');
 
+  categoryMenu.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', event => {
+      event.preventDefault();
+      const categoryName = event.target.textContent;
+      document.getElementById('dropdownMenuButton').textContent = categoryName;
+      const categoryId = event.target.getAttribute('data-category');
+      const type = event.target.getAttribute('data-type');
+      fetchSeries(categoryId, type, 1);
+    });
+  });
+}
 
-
-
+function searchSeries(query) {
+  const encodedQuery = encodeURIComponent(query);
+  const searchUrl = `${apiUrl}/search/tv?api_key=${apiKey}&language=pt-BR&query=${encodedQuery}`;
+  
+  fetch(searchUrl)
+    .then(response => response.json())
+    .then(data => {
+      const results = data.results;
+      displayContent(results, 'series-container');
+    })
+    .catch(error => {
+      console.error('Error searching series:', error);
+    });
+}
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -65,4 +162,3 @@ document.addEventListener("DOMContentLoaded", function () {
     sidebar.classList.toggle('active');
   });
 });
-
