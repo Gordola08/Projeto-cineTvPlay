9edbe098c080e6964f57c2d40e53245f0904ee47
Antonio Parra Neto <116387791+AntonioParraNeto@users.noreply.github.com>
2024-07-17 12:39:17 -0300

    att filmes favoritos
    deixar as funções para deixar os filmes favoritos funcionando

--- cineTvPlay/js/login.js
+++ cineTvPlay/js/login.js
@@ -1,3 +1,7 @@
+const loginForm = document.getElementById('login-form');
+const btnBackLogin = document.getElementById('btn-back-login');
+const btnLogin = document.getElementById('btn-login');
+
 document.addEventListener('DOMContentLoaded', function () {
     const loadingContent = document.getElementById('loading-content');
     const mainContent = document.getElementById('main-content');
@@ -8,114 +12,110 @@ document.addEventListener('DOMContentLoaded', function () {
         mainContent.style.display = 'block';
     }, 2000);
 
-    const loginForm = document.getElementById('login-form');
-    const btnBackLogin = document.getElementById('btn-back-login');
-    const btnLogin = document.getElementById('btn-login');
 
+    fetch('https://cinetv-play-default-rtdb.firebaseio.com/usuario.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro na solicitação.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            let userExists = false;
+            let userKey = null;
 
-      fetch('https://cinetv-play-default-rtdb.firebaseio.com/usuario.json')
-          .then(response => {
-              if (!response.ok) {
-                  throw new Error('Erro na solicitação.');
-              }
-              return response.json();
-          })
-          .then(data => {
-              let userExists = false;
-              let userKey = null;
-
-              for (const key in data) {
-                  if (data[key].email === emailValue && data[key].senha === senhaValue) {
-                      userExists = true;
-                      userKey = key;
-                      // Armazene os detalhes do usuário e a chave no localStorage
-                      localStorage.setItem('usuario_logado', JSON.stringify({ user: data[key], key: key }));
-                      alert('Login bem-sucedido!');
-                      window.location.href = 'cinetv/index.html';
-                      break;
-                  }
-              }
-
-              if (userExists == true) {
-                  alert('Credenciais de login inválidas. Tente novamente.');
-              }
-          })
-          .catch(error => {
-              console.error('Erro na solicitação:', error);
-              alert('Ocorreu um erro ao processar a solicitação. Por favor, tente novamente mais tarde.');
-          });
-  });
-    loginForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-    
-        const emailValue = document.getElementById('email').value;
-        const senhaValue = document.getElementById('senha').value;
-    
-        fetch('https://cinetvplay-eba33-default-rtdb.firebaseio.com/usuario.json')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Erro na solicitação.');
+            for (const key in data) {
+                if (data[key].email === emailValue && data[key].senha === senhaValue) {
+                    userExists = true;
+                    userKey = key;
+                    // Armazene os detalhes do usuário e a chave no localStorage
+                    localStorage.setItem('usuario_logado', JSON.stringify({ user: data[key], key: key }));
+                    alert('Login bem-sucedido!');
+                    window.location.href = 'cinetv/index.html';
+                    break;
                 }
-                return response.json();
-            })
-            .then(data => {
-                let userExists = false;
-                let userKey = null;
-    
-                for (const key in data) {
-                    if (data[key].email === emailValue && data[key].senha === senhaValue) {
-                        userExists = true;
-                        userKey = key;
-                        // Armazena os detalhes do usuário e a chave no localStorage
-                        localStorage.setItem('usuario_logado', JSON.stringify({ user: data[key], key: key }));
-                        // Exibe mensagem de sucesso
-                        const successAlert = document.createElement('div');
-                        successAlert.classList.add('alert', 'alert-success');
-                        successAlert.setAttribute('role', 'alert');
-                        successAlert.textContent = 'Login bem-sucedido!';
-                        document.body.appendChild(successAlert);
-                        // Redireciona para a página após o login
-                        setTimeout(function () {
-                            window.location.href = 'cinetv/index.html';
-                        }, 2000);
-                        
-                        return; // Sai do loop assim que encontrar o usuário válido
-                    }
+            }
+
+            if (userExists == false) {
+                alert('Credenciais de login inválidas. Tente novamente.');
+            }
+        })
+        .catch(error => {
+            console.error('Erro na solicitação:', error);
+            alert('Ocorreu um erro ao processar a solicitação. Por favor, tente novamente mais tarde.');
+        });
+});
+loginForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const emailValue = document.getElementById('email').value;
+    const senhaValue = document.getElementById('senha').value;
+
+    fetch('https://cinetvplay-eba33-default-rtdb.firebaseio.com/usuario.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro na solicitação.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            let userExists = false;
+            let userKey = null;
+
+            for (const key in data) {
+                if (data[key].email === emailValue && data[key].senha === senhaValue) {
+                    userExists = true;
+                    userKey = key;
+                    // Armazena os detalhes do usuário e a chave no localStorage
+                    localStorage.setItem('usuario_logado', JSON.stringify({ user: data[key], key: key }));
+                    // Exibe mensagem de sucesso
+                    const successAlert = document.createElement('div');
+                    successAlert.classList.add('alert', 'alert-success');
+                    successAlert.setAttribute('role', 'alert');
+                    successAlert.textContent = 'Login bem-sucedido!';
+                    document.body.appendChild(successAlert);
+                    // Redireciona para a página após o login
+                    setTimeout(function () {
+                        window.location.href = 'cinetv/index.html';
+                    }, 2000);
+
+                    return; // Sai do loop assim que encontrar o usuário válido
                 }
-                // Se o usuário não existir, exibe mensagem de erro
-                if (!userExists) {
-                    // Exibe mensagem de erro
-                    const errorAlert = document.createElement('div');
-                    errorAlert.classList.add('alert', 'alert-danger');
-                    errorAlert.setAttribute('role', 'alert');
-                    errorAlert.textContent = 'Credenciais de login inválidas. Tente novamente.';
-                    document.body.appendChild(errorAlert);
-                }
-    
-                // Remove o spinner após o processamento
-                btnLogin.innerHTML = 'Login';
-            })
-            .catch(error => {
-                console.error('Erro na solicitação:', error);
-                // Exibe mensagem de erro genérica
-                const genericErrorAlert = document.createElement('div');
-                genericErrorAlert.classList.add('alert', 'alert-danger');
-                genericErrorAlert.setAttribute('role', 'alert');
-                genericErrorAlert.textContent = 'Ocorreu um erro ao processar a solicitação. Por favor, tente novamente mais tarde.';
-                document.body.appendChild(genericErrorAlert);
-    
-                // Remove o spinner após o erro
-                btnLogin.innerHTML = 'Login';
-            });
-    
-        // Exibe o spinner de carregamento no botão de login
-        const spinner = document.createElement('div');
-        spinner.classList.add('spinner-border', 'text-danger');
-        spinner.setAttribute('role', 'status');
-        const spinnerText = document.createElement('span');
-        spinnerText.classList.add('visually-hidden');
-        spinnerText.textContent = 'Loading...';
-        spinner.appendChild(spinnerText);
-        btnLogin.innerHTML = '';
-        btnLogin.appendChild(spinner);
-    });
+            }
+            // Se o usuário não existir, exibe mensagem de erro
+            if (!userExists) {
+                // Exibe mensagem de erro
+                const errorAlert = document.createElement('div');
+                errorAlert.classList.add('alert', 'alert-danger');
+                errorAlert.setAttribute('role', 'alert');
+                errorAlert.textContent = 'Credenciais de login inválidas. Tente novamente.';
+                document.body.appendChild(errorAlert);
+            }
+
+            // Remove o spinner após o processamento
+            btnLogin.innerHTML = 'Login';
+        })
+        .catch(error => {
+            console.error('Erro na solicitação:', error);
+            // Exibe mensagem de erro genérica
+            const genericErrorAlert = document.createElement('div');
+            genericErrorAlert.classList.add('alert', 'alert-danger');
+            genericErrorAlert.setAttribute('role', 'alert');
+            genericErrorAlert.textContent = 'Ocorreu um erro ao processar a solicitação. Por favor, tente novamente mais tarde.';
+            document.body.appendChild(genericErrorAlert);
+
+            // Remove o spinner após o erro
+            btnLogin.innerHTML = 'Login';
+        });
+
+    // Exibe o spinner de carregamento no botão de login
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner-border', 'text-danger');
+    spinner.setAttribute('role', 'status');
+    const spinnerText = document.createElement('span');
+    spinnerText.classList.add('visually-hidden');
+    spinnerText.textContent = 'Loading...';
+    spinner.appendChild(spinnerText);
+    btnLogin.innerHTML = '';
+    btnLogin.appendChild(spinner);
+});
